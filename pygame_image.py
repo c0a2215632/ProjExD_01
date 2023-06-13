5b1a22584b9ff060b666050aa56b58033bd503a9
Kohei Wada <c0a2215632@edu.teu.ac.jp>
2023-06-13 16:16:30 +0900

    練習1～5

---
+++ pygame_image.py
@@ -0,0 +1,31 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    kk_img = pg.image.load("ex01-20230613/fig/3.png") 
+    kt_img = pg.transform.flip(kk_img, True,False)
+    bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
+    ku_img = pg.transform.rotozoom(kk_img,10,1.0)
+    l=[kt_img,ku_img]
+    tmr = 0
+    
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kt_img,[300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
